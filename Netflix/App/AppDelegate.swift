280e2312e0e313d1df6d176b12ecbfaa69e7ef68
Serdar Turan <trnserdar@gmail.com>
2020-12-19 16:59:55 +0300

    improvements about file structure.

---
+++ Netflix/App/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Netflix
+//
+//  Created by SERDAR TURAN on 19.12.2020.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        let rootViewController = TabBarViewController()
+        let navigationController = UINavigationController(rootViewController: rootViewController)
+        self.window?.rootViewController = navigationController
+        self.window?.makeKeyAndVisible()
+        
+        return true
+    }
+
+}
+
---
+++ Netflix/ViewControllers/TabBarViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TabBarViewController.swift
+//  Netflix
+//
+//  Created by SERDAR TURAN on 19.12.2020.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .blue
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
