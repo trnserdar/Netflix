b8a1172d49079cdb2654be98c86337acb3654a23
Serdar Turan <trnserdar@gmail.com>
2020-12-20 20:26:26 +0300

    improvements about genres view controller.

--- Netflix/ViewControllers/GenresViewController.swift
+++ Netflix/ViewControllers/GenresViewController.swift
@@ -8,23 +8,108 @@
 import UIKit
 
 class GenresViewController: UIViewController {
+    
+    lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.backgroundColor = StyleConstants.Color.lightGray
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.keyboardDismissMode = .onDrag
+        return tableView
 
+    }()
+    
+    lazy var searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        searchBar.backgroundColor = StyleConstants.Color.lightGray
+        searchBar.isTranslucent = true
+        searchBar.searchBarStyle = .prominent
+        searchBar.placeholder = " Search..."
+        searchBar.sizeToFit()
+        searchBar.showsCancelButton = true
+        searchBar.returnKeyType = .search
+        searchBar.delegate = self
+        return searchBar
+    }()
+
+    var sources: [String] = ["A", "B", "C"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .red
-        // Do any additional setup after loading the view.
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillLayoutSubviews() {
+        configureSearchBar()
+        configureTableView()
     }
-    */
+    
+    func configureSearchBar() {
+        view.addSubview(searchBar)
+        NSLayoutConstraint.activate([
+            searchBar.leftAnchor.constraint(equalTo: view.leftAnchor),
+            searchBar.rightAnchor.constraint(equalTo: view.rightAnchor),
+            searchBar.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor)
+        ])
+        
+    }
+    
+    func configureTableView() {
+        view.addSubview(tableView)
+        NSLayoutConstraint.activate([
+            tableView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            tableView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            tableView.topAnchor.constraint(equalTo: searchBar.bottomAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
+        
+    }
+    
+}
 
+extension GenresViewController: UISearchBarDelegate {
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print("GenresViewController searchText: \(searchText)")
+    }
+    
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        print("GenresViewController searchBarTextDidEndEditing: \(searchBar.text)")
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.endEditing(true)
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.endEditing(true)
+    }
+}
+
+extension GenresViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("GenresViewController didSelect: \(indexPath.row)")
+    }
+}
+
+extension GenresViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sources.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.backgroundColor = StyleConstants.Color.lightGray
+        cell.selectionStyle = .none
+        cell.textLabel?.text = sources[indexPath.row]
+        return cell
+    }
 }
