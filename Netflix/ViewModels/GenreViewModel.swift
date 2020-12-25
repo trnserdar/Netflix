1c16a8651719421d1b35ce563c3485109d3c46fa
Serdar Turan <trnserdar@gmail.com>
2020-12-25 16:44:55 +0300

    improvements about genre view model.

--- Netflix/ViewModels/GenreViewModel.swift
+++ Netflix/ViewModels/GenreViewModel.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import UIKit
 
 struct GenreViewModel {
     
@@ -21,5 +22,21 @@ struct GenreViewModel {
     
     var selectedId: String {
         return "\(genre.ids?.first ?? 0)"
+    }
+    
+    var itemWidth: CGFloat {
+        return textWidth(font: StyleConstants.Font.body!, text: nameText) + 16
+    }
+    
+    var itemHeight: CGFloat {
+        return 30.0
+    }
+    
+    func textWidth(font: UIFont, text: String) -> CGFloat {
+        let myText = text as NSString
+        
+        let rect = CGSize(width: CGFloat.greatestFiniteMagnitude, height: CGFloat.greatestFiniteMagnitude)
+        let labelSize = myText.boundingRect(with: rect, options: .usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font: font], context: nil)
+        return ceil(labelSize.width)
     }
 }
