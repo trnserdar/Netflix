697066b49ab82de698161be3ca945b32b9cb0129
Serdar Turan <trnserdar@gmail.com>
2020-12-25 16:49:43 +0300

    added result detailing protocol for coordinators.

---
+++ Netflix/Coordinator/Protocols/ResultDetailing.swift
@@ -0,0 +1,12 @@
+//
+//  ResultDetailing.swift
+//  Netflix
+//
+//  Created by SERDAR TURAN on 25.12.2020.
+//
+
+import Foundation
+
+protocol ResultDetailing: AnyObject {
+    func showResultDetail(result: SearchResult)
+}
