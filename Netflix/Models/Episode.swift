6e44a52458755fbc6df4fc0c928132d4be837402
Serdar Turan <trnserdar@gmail.com>
2020-12-26 23:48:44 +0300

    added equatable conformances.

--- Netflix/Models/Episode.swift
+++ Netflix/Models/Episode.swift
@@ -15,17 +15,38 @@ struct EpisodeResponse: Codable {
     }
 }
 
-struct EpisodeResult: Codable {
+struct EpisodeResult: Codable, Equatable {
     let seasid, seasnum, country: String?
     let episodes: [EpisodeElement]?
+    
+    static func == (lhs: EpisodeResult, rhs: EpisodeResult) -> Bool {
+        return lhs.seasid == rhs.seasid &&
+            lhs.seasnum == rhs.seasnum &&
+            lhs.country == rhs.country &&
+            lhs.episodes == rhs.episodes
+    }
 }
 
-struct EpisodeElement: Codable {
+struct EpisodeElement: Codable, Equatable {
     let episode: Episode?
+    
+    static func == (lhs: EpisodeElement, rhs: EpisodeElement) -> Bool {
+        return lhs.episode == rhs.episode
+    }
 }
 
-struct Episode: Codable {
+struct Episode: Codable, Equatable {
     let id, seasnum, epnum, title: String?
     let image: String?
     let synopsis, available: String?
+    
+    static func == (lhs: Episode, rhs: Episode) -> Bool {
+        return lhs.id == rhs.id &&
+            lhs.seasnum == rhs.seasnum &&
+            lhs.epnum == rhs.epnum &&
+            lhs.title == rhs.title &&
+            lhs.image == rhs.image &&
+            lhs.synopsis == rhs.synopsis &&
+            lhs.available == rhs.available
+    }
 }
