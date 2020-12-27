792236fbaf09ee9ceaa6c874221ef32888290156
Serdar Turan <trnserdar@gmail.com>
2020-12-27 16:42:59 +0300

    improvements about back arrow color in main navigation controller.

--- Netflix/ViewControllers/Base/MainNavigationController.swift
+++ Netflix/ViewControllers/Base/MainNavigationController.swift
@@ -38,6 +38,7 @@ extension MainNavigationController: UINavigationControllerDelegate {
     
     func navigationController(_ navigationController: UINavigationController, willShow viewController: UIViewController, animated: Bool) {
         let item = UIBarButtonItem(title: " ", style: .plain, target: nil, action: nil)
+        item.tintColor = StyleConstants.Color.turquoise
         viewController.navigationItem.backBarButtonItem = item
     }
 }
