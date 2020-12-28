5ad69502b4f402517232bf45f4868dfc86ba6ed2
Serdar Turan <trnserdar@gmail.com>
2020-12-28 16:39:41 +0300

    added user defaults key constants struct.

---
+++ Netflix/SupportingFiles/Constants/UserDefaultsKeyConstants.swift
@@ -0,0 +1,12 @@
+//
+//  UserDefaultsKeyConstants.swift
+//  Netflix
+//
+//  Created by SERDAR TURAN on 28.12.2020.
+//
+
+import Foundation
+
+struct UserDefaultsKeyConstants {
+    static let favorites = "favorites"
+}
