6fe704bd34506e45a34740103b28a99a653f1c71
Serdar Turan <trnserdar@gmail.com>
2021-01-06 17:50:23 +0300

    added test helpers.

---
+++ NetflixTests/HelperClasses/TestHelpers.swift
@@ -0,0 +1,12 @@
+//
+//  TestHelpers.swift
+//  NetflixTests
+//
+//  Created by SERDAR TURAN on 6.01.2021.
+//
+
+import XCTest
+
+func executeRunLoop() {
+    RunLoop.current.run(until: Date())
+}
