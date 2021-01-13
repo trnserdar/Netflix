d774880a0b80137f62d374b51718d51cf6ede7f9
Serdar Turan <trnserdar@gmail.com>
2021-01-14 01:40:21 +0300

    added test api key.

--- Netflix/SupportingFiles/Constants/NetworkConstants.swift
+++ Netflix/SupportingFiles/Constants/NetworkConstants.swift
@@ -9,6 +9,6 @@ import Foundation
 
 struct NetworkConstants {
     static var baseURL = "https://unogs-unogs-v1.p.rapidapi.com/aaapi.cgi"
-    static var apiKey = ""
+    static var apiKey = "46bfc16e82msh96239f52bd87724p102c67jsn7c7bce8c6d9e"
     static var apiHost = "unogs-unogs-v1.p.rapidapi.com"
 }
