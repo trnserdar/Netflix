c41d254af58faed6d63b08a0ad0f7b172a4b9c9c
Serdar Turan <trnserdar@gmail.com>
2021-01-13 17:46:51 +0300

    improvements about mock coordinator.

--- NetflixTests/Mocks/MockCoordinator.swift
+++ NetflixTests/Mocks/MockCoordinator.swift
@@ -8,19 +8,11 @@
 import XCTest
 @testable import Netflix
 
-class MockCoordinator: Coordinator, SearchResulting, ResultDetailing, CastDetailing {
+class MockCoordinator: SearchResulting, ResultDetailing, CastDetailing {
    
-    var childCoordinators: [Coordinator] = []
-    var navigationController: UINavigationController = MainNavigationController()
-
-    var startCount = 0
     var showResultCount = 0
     var showResultDetailCount = 0
     var showCastDetailingCount = 0
-    
-    func start() {
-        startCount += 1
-    }
     
     func showResult(navigationTitle: String?, results: [SearchResult]) {
         showResultCount += 1
